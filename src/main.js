e4c49a2a4d28422775dc97604cac31d3ede30c4d
siloong <2296926656@qq.com>
2020-09-16 17:30:13 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// css
+import('@/styles/index.less');  //global style
+
 Vue.config.productionTip = false
 
 new Vue({
