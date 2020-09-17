ddf765fbd18d1d13ffc9ac9f48aea6c89ff1a05b
siloong <2296926656@qq.com>
2020-09-17 12:01:00 +0800

    pxtorem

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// lib
+import Vant from 'vant'; //vant component
+import 'amfe-flexible'; //flex setting html font-size
+
 // css
-import('@/styles/index.less');  //global style
+import '@/styles/index.less';  //global style
+import 'vant/lib/index.css';  //vant component style
+
+// use
+Vue.use(Vant);  //use vant component
 
 Vue.config.productionTip = false
 
